1e6b10670c61ff099a3f053e0974a03a1e4eaf7c
cacdnaeast <anand.chauhan@ca.com>
2017-06-20 23:22:46 -0400

    Update todo-spec.js to v2
    Updated elements to match Keith's version: https://github.com/keithpuzey/CARaffle/blob/master/Protractorv2/todo-spec.js

--- protractor/todo-spec.js
+++ protractor/todo-spec.js
@@ -2,7 +2,7 @@ describe('The Modern Software Factory Demo UI Test - NA East', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://naeast-preprod.cdbu.io');
-    element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
-    element(by.buttonText('Submit')).click();
+    element(by.css('[name="name"]')).sendKeys('Selenium Test User');
+    element(by.buttonText('I\'m In!')).click();
     });
 });
