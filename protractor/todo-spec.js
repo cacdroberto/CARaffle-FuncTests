f7dcc360a0dea8b9f794b854c4e8a8d8e72d8bd9
cacdnaeast <anand.chauhan@ca.com>
2017-06-20 23:11:38 -0400

    Update todo-spec.js
    Updated preprod url and description

--- protractor/todo-spec.js
+++ protractor/todo-spec.js
@@ -1,7 +1,7 @@
-describe('CDBU Modern Software Factory UI Test - xxx Environment', function() {
+describe('The Modern Software Factory Demo UI Test - NA East', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
-    browser.get('http://<preprod_url>');
+    browser.get('http://naeast-preprod.cdbu.io');
     element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
     element(by.buttonText('Submit')).click();
     });
