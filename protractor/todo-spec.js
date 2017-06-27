8422958e7f58b182757322edab43eb23fc7dd5d0
cacdroberto <roberto.conto@ca.com>
2017-06-27 10:24:36 -0400

    Update todo-spec.js

--- protractor/todo-spec.js
+++ protractor/todo-spec.js
@@ -1,7 +1,7 @@
-describe('The Modern Software Factory Demo UI Test - NA East', function() {
+describe('The Modern Software Factory Demo UI Test - NY', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
-    browser.get('http://naeast-preprod.cdbu.io');
+    browser.get('http://ny-preprod.cdbu.io/');
     element(by.css('[name="name"]')).sendKeys('Selenium Test User');
     element(by.buttonText('I\'m In!')).click();
     });
